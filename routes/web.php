caaa9e753f221ea8b31ee1fdc08f5c4706db6a9b
Caião <caiobges@hotmail.com>
2024-01-02 10:29:43 -0300

    [ADD] views

--- routes/web.php
+++ routes/web.php
@@ -24,50 +24,54 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth'], function () {
 
+});
+    Route::get('/prospecto',function (){
+      return view('gestao-oportunidade.prospecto');
+    });
+
     Route::get('/', [HomeController::class, 'home']);
 	Route::get('dashboard', function () {
-		return view('dashboard');
-	})->name('dashboard');
+        return view('dashboard');
+    })->name('dashboard');
 
 	Route::get('billing', function () {
-		return view('billing');
-	})->name('billing');
+        return view('billing');
+    })->name('billing');
 
 	Route::get('profile', function () {
-		return view('profile');
-	})->name('profile');
+        return view('profile');
+    })->name('profile');
 
 	Route::get('rtl', function () {
-		return view('rtl');
-	})->name('rtl');
+        return view('rtl');
+    })->name('rtl');
 
 	Route::get('user-management', function () {
-		return view('laravel-examples/user-management');
-	})->name('user-management');
+        return view('laravel-examples/user-management');
+    })->name('user-management');
 
 	Route::get('tables', function () {
-		return view('tables');
-	})->name('tables');
+        return view('tables');
+    })->name('tables');
 
     Route::get('virtual-reality', function () {
-		return view('virtual-reality');
-	})->name('virtual-reality');
+        return view('virtual-reality');
+    })->name('virtual-reality');
 
     Route::get('static-sign-in', function () {
-		return view('static-sign-in');
-	})->name('sign-in');
+        return view('static-sign-in');
+    })->name('sign-in');
 
     Route::get('static-sign-up', function () {
-		return view('static-sign-up');
-	})->name('sign-up');
+        return view('static-sign-up');
+    })->name('sign-up');
 
     Route::get('/logout', [SessionsController::class, 'destroy']);
 	Route::get('/user-profile', [InfoUserController::class, 'create']);
 	Route::post('/user-profile', [InfoUserController::class, 'store']);
     Route::get('/login', function () {
-		return view('dashboard');
-	})->name('sign-up');
-});
+        return view('dashboard');
+    })->name('sign-up');
 
 
 

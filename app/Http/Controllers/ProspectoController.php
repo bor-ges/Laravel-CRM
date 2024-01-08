9d7013abbbaeed3b218165fd4ace6bd150c3fd9e
CAIÃO <caiobges@hotmail.com>
2024-01-08 15:45:06 -0300

    [ADD]CRUD completo

--- app/Http/Controllers/ProspectoController.php
+++ app/Http/Controllers/ProspectoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Prospecto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ProspectoController extends Controller
 {
@@ -12,7 +14,9 @@ class ProspectoController extends Controller
      */
     public function index()
     {
-        //
+        $prospecto = DB::table('prospecto')->get();
+
+        return view('gestao-oportunidade.prospecto', compact('prospecto'));
     }
 
     /**
@@ -20,7 +24,7 @@ class ProspectoController extends Controller
      */
     public function create()
     {
-        //
+        return view('gestao-oportunidade.prospecto');
     }
 
     /**
@@ -28,7 +32,34 @@ class ProspectoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'nome_cliente' => 'required|max:4294967295',
+            'conhecimento' => 'required|max:4294967295',
+            'origem' => 'required|max:4294967295',
+            'nome_oportunidade' => 'required|max:4294967295',
+            'tipo_oportunidade' => 'required|max:4294967295',
+            'data' => 'required|numeric|max:255',
+            'estimado' => 'required|numeric|max:255',
+            'probabilidade' => 'required|max:4294967295',
+            'proximo' => 'required|max:4294967295',
+
+        ]);
+
+        $prospecto = new Prospecto();
+
+        $prospecto->nome_cliente = $validated['nome_cliente'];
+        $prospecto->conhecimento = $validated['conhecimento'];
+        $prospecto->origem = $validated['origem'];
+        $prospecto->nome_oportunidade = $validated['nome_oportunidade'];
+        $prospecto->tipo_oportunidade = $validated['tipo_oportunidade'];
+        $prospecto = Carbon::parse($validated['prospecto'])->format('Y/m/d');
+        $prospecto->estimado = $validated['estimado'];
+        $prospecto->probabilidade = $validated['probabilidade'];
+        $prospecto->proximo = $validated['proximo'];
+
+        $prospecto->save();
+
+        return redirect()->route('dashboard');
     }
 
     /**
@@ -50,9 +81,34 @@ class ProspectoController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Prospecto $prospecto)
+    public function update(Request $request, Prospecto $prospecto): \Illuminate\Http\RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'nome_cliente' => 'required|max:4294967295',
+            'conhecimento' => 'required|max:4294967295',
+            'origem' => 'required|max:4294967295',
+            'nome_oportunidade' => 'required|max:4294967295',
+            'tipo_oportunidade' => 'required|max:4294967295',
+            'data' => 'required|numeric|max:255',
+            'estimado' => 'required|numeric|max:255',
+            'probabilidade' => 'required|max:4294967295',
+            'proximo' => 'required|max:4294967295',
+
+        ]);
+
+        $prospecto->nome_cliente = $validated['nome_cliente'];
+        $prospecto->conhecimento = $validated['conhecimento'];
+        $prospecto->origem = $validated['origem'];
+        $prospecto->nome_oportunidade = $validated['nome_oportunidade'];
+        $prospecto->tipo_oportunidade = $validated['tipo_oportunidade'];
+        $prospecto->data = $validated['data'];
+        $prospecto->estimado = $validated['estimado'];
+        $prospecto->probabilidade = $validated['probabilidade'];
+        $prospecto->proximo = $validated['proximo'];
+
+        $prospecto->save();
+
+        return redirect()->route('dashboard');
     }
 
     /**
@@ -60,6 +116,7 @@ class ProspectoController extends Controller
      */
     public function destroy(Prospecto $prospecto)
     {
-        //
+        $prospecto->delete();
+        return redirect()->route('dashboard');
     }
 }

12b833c10a8c108de22252dc7d20894b109d48f3
CAIÃO <caiobges@hotmail.com>
2024-02-05 16:04:36 -0300

    [ADD]mudança view

--- resources/views/gestao-oportunidade/prospecto.blade.php
+++ resources/views/gestao-oportunidade/prospecto.blade.php
@@ -17,17 +17,17 @@
             <div class="row">
                 <div class="col-md-3">
                     <label for="nome_cliente">Nome do Cliente:</label>
-                    <input id="nome_cliente" value="{{$prospecto->nome_cliente}}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    <input name="nome_cliente" id="nome_cliente" value="" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                 </div>
 
                 <div class="col-md-3">
                     <label for="conhecimento">Conhecimento sobre o cliente:</label>
-                    <input id="conhecimento" value="{{$prospecto->conhecimento}}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    <input name="conhecimento" id="conhecimento" value="" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                 </div>
 
                 <div class="col-md-6">
                     <label for="descricao">Origem da oportunidade:</label>
-                    <input id="nome_cliente" value="{{$prospecto->origem}}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    <input name="origem" id="descricao" value="" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                 </div>
 
             </div>
@@ -35,29 +35,29 @@
             <div class="row">
                 <div class="col-md-4">
                     <label for="oportunidade">Nome da oportunidade:</label>
-                    <input id="oportunidade" value="{{$prospecto->nome_oportunidade}}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    <input name="nome_oportunidade" id="oportunidade" value="" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                 </div>
                 <div class="col-md-2">
-                    <label for="descricao">Tipo da oportunidade:</label>
-                    <input id="tipo_oportunidade" value="{{$prospecto->tipo_oportunidade}}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    <label for="tipo_oportunidade">Tipo da oportunidade:</label>
+                    <input name="tipo_oportunidade" id="tipo_oportunidade" value="" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                 </div>
                 <div class="col-md-3">
                     <label for="datepick">Data inicio e fim:</label>
-                    <input id="datepick" value="{{$prospecto->data}}" class="form-control datepicker" placeholder="Please select date" type="text" onfocus="focused(this)" onfocusout="defocused(this)">
+                    <input name="data" id="datepick" value="" class="form-control datepicker" placeholder="Please select date" type="text" onfocus="focused(this)" onfocusout="defocused(this)">
                 </div>
                 <div class="col-md-3">
                     <label for="valor_estimado">Valor Estimado:</label>
-                    <input id="valor_estimado" value="{{$prospecto->estimado}}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    <input name="estimado" id="valor_estimado" value="" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                 </div>
             </div>
             <div class="row mt-2">
                 <div class="col-md-3">
                     <label for="prob_fechamento">Probabilidade de fechamento:</label>
-                    <input id="prob_fechamento" value="{{$prospecto->probabilidade}}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    <input name="probabilidade" id="prob_fechamento" value="" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                 </div>
                 <div class="col-md-3">
                     <label for="proximo_passo">Proximo passo:</label>
-                    <input id="proximo_passo" value="{{$prospecto->proximo}}" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    <input name="proximo" id="proximo" value="" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                 </div>
                 <div class="col-md-4 mt-4">
                     <button type="submit" class="btn btn-info btn-lg align-itens-end">Confirmar cadastro</button>

c2b8aaa5c64f15f8888acfdaa47c9570eb5b1cce
CAIÃO <caiobges@hotmail.com>
2024-02-28 15:56:42 -0300

    [change]coisas aleatorias

--- resources/views/gestao-oportunidade/abordagem.blade.php
+++ resources/views/gestao-oportunidade/abordagem.blade.php
@@ -62,6 +62,7 @@
                                 <th class="text-secondary opacity-7"></th>
                             </tr>
                             </thead>
+                            @foreach()
                             <tbody>
                             <tr>
                                 <td>
@@ -83,32 +84,30 @@
                                     </a>
                                 </td>
                             </tr>
-
-
-                                <div class="modal fade" id="modal-default" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
-                                    <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h6 class="modal-title" id="modal-title-default">Informações</h6>
-                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">×</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <!-- Adicionando campos no corpo do modal -->
-                                                <p><strong>Nome:</strong> teste</p>
-                                                <p><strong>Tipo:</strong> teste</p>
-                                                <p><strong>Data de Realização:</strong> teste</p>
-                                                <p><strong>Descrição:</strong> teste</p>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-danger  ml-auto" data-bs-dismiss="modal">Fechar</button>
-                                            </div>
+                            <div class="modal fade" id="modal-default" tabindex="-1" role="dialog" aria-labelledby="modal-default" aria-hidden="true">
+                                <div class="modal-dialog modal- modal-dialog-centered modal-" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h6 class="modal-title" id="modal-title-default">Informações</h6>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">×</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <!-- Adicionando campos no corpo do modal -->
+                                            <p><strong>Nome:</strong> teste</p>
+                                            <p><strong>Tipo:</strong> teste</p>
+                                            <p><strong>Data de Realização:</strong> teste</p>
+                                            <p><strong>Descrição:</strong> teste</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-danger  ml-auto" data-bs-dismiss="modal">Fechar</button>
                                         </div>
                                     </div>
-
+                                </div>
                             </div>
                             </tbody>
+                            @endforeach
                         </table>
                     </div>
                 </div>

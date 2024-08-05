0b956a9cc0ab46acbf4473758e92d2f3308a74bd
CAIÃO <caiobges@hotmail.com>
2024-08-05 16:25:02 -0300

    modificação navbar
    Co-Authored-By: Lucas Souza <90305152+lucasbenacci@users.noreply.github.com>

--- resources/views/layouts/footers/guest/footer.blade.php
+++ resources/views/layouts/footers/guest/footer.blade.php
@@ -1,4 +1,3 @@
-  <!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
   <footer class="footer py-5">
     <div class="container">
       <div class="row">
@@ -26,4 +25,3 @@
       @endif
     </div>
   </footer>
-  <!-- -------- END FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
--- resources/views/layouts/navbars/auth/nav.blade.php
+++ resources/views/layouts/navbars/auth/nav.blade.php
@@ -9,12 +9,6 @@
 {{--            <h6 class="font-weight-bolder mb-0 text-capitalize">{{ str_replace('-', ' ', Request::path()) }}</h6>--}}
 {{--        </nav>--}}
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4 d-flex justify-content-end" id="navbar">
-{{--            <div class="ms-md-3 pe-md-3 d-flex align-items-center">--}}
-{{--            <div class="input-group">--}}
-{{--                <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>--}}
-{{--                <input type="text" class="form-control" placeholder="Pesquisar...">--}}
-{{--            </div>--}}
-{{--            </div>--}}
             <ul class="navbar-nav  justify-content-end">
                 <li class="nav-item d-flex align-items-center">
                     <a href="{{ url('/logout')}}" class="nav-link text-body font-weight-bold px-0">
